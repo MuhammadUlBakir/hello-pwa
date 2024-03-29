82710af357826a850397f9fdefbe31983bf4b1cd
Muhammad-ul-bakir <bakirmuhammad74@gmail.com>
2024-03-29 16:00:26 +0500

    add some changes

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ customInstallButton.addEventListener('click', (event) => {
 // Service Worker Registration
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+    navigator.serviceWorker.register('/service-worker.js/').then(function(registration) {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
       console.error('ServiceWorker registration failed: ', err);

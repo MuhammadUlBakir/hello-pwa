add3221a65cb605d68a4111db3378ee05f7e472e
Muhammad-ul-bakir <bakirmuhammad74@gmail.com>
2024-04-01 12:29:57 +0500

    add some changes

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 var cacheName = 'hello-pwa';
 var filesToCache = [
   '/',
-  '/index.html',
+  '/test.html',
   '/css/style.css',
-  '/js/main.js'
+  '/js/main.js',
+  '/images/test.png'
 ];
 
-/* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
@@ -16,11 +16,16 @@ self.addEventListener('install', function(e) {
   self.skipWaiting();
 });
 
-/* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+    caches.open(cacheName).then(function(cache) {
+      return cache.match(e.request).then(function(response) {
+        var fetchPromise = fetch(e.request).then(function(networkResponse) {
+          cache.put(e.request, networkResponse.clone());
+          return networkResponse;
+        });
+        return response || fetchPromise;
+      });
     })
   );
 });
